08a456860d38e9de012432b7dff0b698b636aa64
Jesus Sulca Ciprian <81652950+JesusSulca@users.noreply.github.com>
2022-07-20 16:36:40 -0500

    Update

--- dbPizzaHut.sql
+++ dbPizzaHut.sql
@@ -1,3 +1,4 @@
+
 CREATE DATABASE bdPizzaHut
 GO
 
@@ -24,16 +25,16 @@ Go
 INSERT INTO CLIENTE
 (NOMCLI ,APECLI ,EMACLI ,CELCLI,DNICLI ,DIRCLI, ESTCLI,FECNACCLI,CODUBI,IDLOG)
 VALUES
-('Pepe Juan','Castro Sanchez','p.castrosanchez@gmail.com','975364937', '15347765','Av. 28 de Julio N° 100','A','13-03-1999','140406',6),
+('Pepe Juan','Castro Sanchez','p.castrosanchez@gmail.com','975364937', '15347765','Av. 28 de Julio NÂ° 100','A','13-03-1999','140406',6),
 ('Juana Yuliana','Tacza Saavedra','j.tacza@gmail.com','999364937', '15347766','Av. 2 de mayo','A','14-04-2000','140406',11),
-('Jenny','Urdaneta Alfaro','j.urdaneta@gmail.com','975364938', '15347767','Av. ramos larrea N° 101','A','13-04-1999','140406',12),
-('Josimar','Bernal Casas','j.bernal@gmail.com','975364939', '15347768','Av. sucre N° 90','A','13-03-1999','140406',13),
-('Rocio','Malpartida Osorio','r.malpartida@gmail.com','975364939', '15347769','Av. ramos N° 800','A','13-03-1999','140406',14),
-('Lady','Ramos Ramos','l.ramos@gmail.com','975364940', '15347770','Av. Ayacucho N° 190','A','13-03-1999','140410',15),
-('Alexis Raul','Flores Sanchez','a.flores@gmail.com','975364941', '15347771','Av. la mar N° 200','A','13-03-1999','140410',16),
-('Ronny Raul','Flores Sanchez','r.flores@gmail.com','975364942', '15347772','Av. Canada N° 100','A','13-03-1999','140409',17),
-('Lisset Karina','Ciprian Sanchez','l.ciprian@gmail.com','975364900', '15347773','Av. ayacucho N° 140','A','13-03-1999','140409',18),
-('Ignacio Ernesto','Sulca Pomalaya','i.sulca@gmail.com','975364901', '15347774','Av. principal N° 401','A','13-03-1999','140409',19)
+('Jenny','Urdaneta Alfaro','j.urdaneta@gmail.com','975364938', '15347767','Av. ramos larrea NÂ° 101','A','13-04-1999','140406',12),
+('Josimar','Bernal Casas','j.bernal@gmail.com','975364939', '15347768','Av. sucre NÂ° 90','A','13-03-1999','140406',13),
+('Rocio','Malpartida Osorio','r.malpartida@gmail.com','975364939', '15347769','Av. ramos NÂ° 800','A','13-03-1999','140406',14),
+('Lady','Ramos Ramos','l.ramos@gmail.com','975364940', '15347770','Av. Ayacucho NÂ° 190','A','13-03-1999','140410',15),
+('Alexis Raul','Flores Sanchez','a.flores@gmail.com','975364941', '15347771','Av. la mar NÂ° 200','A','13-03-1999','140410',16),
+('Ronny Raul','Flores Sanchez','r.flores@gmail.com','975364942', '15347772','Av. Canada NÂ° 100','A','13-03-1999','140409',17),
+('Lisset Karina','Ciprian Sanchez','l.ciprian@gmail.com','975364900', '15347773','Av. ayacucho NÂ° 140','A','13-03-1999','140409',18),
+('Ignacio Ernesto','Sulca Pomalaya','i.sulca@gmail.com','975364901', '15347774','Av. principal NÂ° 401','A','13-03-1999','140409',19)
 Go
 
 -- Table: LOGINX
@@ -101,14 +102,14 @@ Go
 INSERT INTO PRODUCTO
 (CODPRO ,TIPPRO ,NOMPRO, PREPRO , TAMPRO , STOCKPRO ,ESTPRO,DETPRO)
 VALUES
-('P0001','P','PIZZA CARBONARA PASSIONE',40,'F',150,'A','La tradicional receta carbonara con jamón, tocino, champiñones y salsa alfredo.'),
+('P0001','P','PIZZA CARBONARA PASSIONE',40,'F',150,'A','La tradicional receta carbonara con jamÃ³n, tocino, champiÃ±ones y salsa alfredo.'),
 ('P0002','P','PIZZA CHICKEN AMORE',15,'I',150,'A','Una pizza con pollo a la parilla, tomates frescos y cebolla morada, con un toque de salsa alfredo.'),
-('P0003','P','PIZZA PEPPERONI ROMANZZA',70,'X',150,'A','De pepperoni, acompañado con tomates frescos y champiñones .'),
-('P0004','P','4 ESTACIONES',32,'F',150,'M','Pizza: súper suprema, canadiense, pepperoni y jamón.'),
-('P0005','P','SUPER SUPREMA',45,'F',150,'F','Hecho con pepperoni, carne de res y cerdo.'),
-('P0006','P','CANADIENSE',48,'F',150,'F','Elaborada con pepperoni, jamón, carne de res y cerdo, salchicha italiana y bacon bits..'),
-('P0007','P','SUPREMA',51,'F',150,'F','Elaborada con pepperoni, carne de res y cerdo, champiñones, chile verde y cebolla.'),
-('P0008','P','PEPPERONI O JAMÓN LOVERS',39,'F',150,'A','Pepperoni o jamón en abundancia, queso 100% mozzarella.');
+('P0003','P','PIZZA PEPPERONI ROMANZZA',70,'A',150,'A','De pepperoni, acompaÃ±ado con tomates frescos y champiÃ±ones .'),
+('P0004','P','4 ESTACIONES',32,'F',150,'A','Pizza: sÃºper suprema, canadiense, pepperoni y jamÃ³n.'),
+('P0005','P','SUPER SUPREMA',45,'F',150,'A','Hecho con pepperoni, carne de res y cerdo.'),
+('P0006','P','CANADIENSE',48,'F',150,'A','Elaborada con pepperoni, jamÃ³n, carne de res y cerdo, salchicha italiana y bacon bits..'),
+('P0007','P','SUPREMA',51,'F',150,'A','Elaborada con pepperoni, carne de res y cerdo, champiÃ±ones, chile verde y cebolla.'),
+('P0008','P','PEPPERONI O JAMÃ“N LOVERS',39,'F',150,'A','Pepperoni o jamÃ³n en abundancia, queso 100% mozzarella.');
 Go
 
 
@@ -126,9 +127,9 @@ GO
 INSERT INTO SUCURSAL
 (NOMSU,DIRSU,CODUBI)
 VALUES
-('Pizza Hut Mega plaza','Benavides n° 154','140410'),
-('Pizza Hut Imperial','2 de mayo n° 754','140406'),
-('Pizza Hut - Nuevo Imperial', 'Av. principal n° 474', '140409');
+('Pizza Hut Mega plaza','Benavides nÂ° 154','140410'),
+('Pizza Hut Imperial','2 de mayo nÂ° 754','140406'),
+('Pizza Hut - Nuevo Imperial', 'Av. principal nÂ° 474', '140409');
 Go
 
 -- Table: UBIGEO
@@ -144,15 +145,15 @@ Go
 INSERT INTO UBIGEO
 (CODUBI,DEPA,PROV,DIST)
 VALUES
-('140402','LIMA','CAÑETE','CALANGO'),
-('140403','LIMA','CAÑETE','CERRO AZUL'),
-('140404','LIMA','CAÑETE','CHILCA'),
-('140405','LIMA','CAÑETE','COAYLLO'),
-('140406','LIMA','CAÑETE','IMPERIAL'),
-('140407','LIMA','CAÑETE','LUNAHUANA'),
-('140408','LIMA','CAÑETE','MALA'),
-('140409','LIMA','CAÑETE','NUEVO IMPERIAL'),
-('140410','LIMA','CAÑETE','SAN VICENTE');
+('140402','LIMA','CAÃ‘ETE','CALANGO'),
+('140403','LIMA','CAÃ‘ETE','CERRO AZUL'),
+('140404','LIMA','CAÃ‘ETE','CHILCA'),
+('140405','LIMA','CAÃ‘ETE','COAYLLO'),
+('140406','LIMA','CAÃ‘ETE','IMPERIAL'),
+('140407','LIMA','CAÃ‘ETE','LUNAHUANA'),
+('140408','LIMA','CAÃ‘ETE','MALA'),
+('140409','LIMA','CAÃ‘ETE','NUEVO IMPERIAL'),
+('140410','LIMA','CAÃ‘ETE','SAN VICENTE');
 GO
 
 -- Table: VENDEDOR
@@ -175,16 +176,16 @@ Go
 INSERT INTO VENDEDOR
 (NOMVEND ,APEVEND ,EMAVEND ,CELVEND,DNIVEND,DIRVEND ,ESTVEND, FECNACVEND,CODUBI,IDLOG)
 VALUES
-('Juan Pepe','Sanchez Castro','juan.sanchez@gmail.com','991932288', '70782063','Av. 28 de Julio N° 150','A','13-03-1999','140406',20),
+('Juan Pepe','Sanchez Castro','juan.sanchez@gmail.com','991932288', '70782063','Av. 28 de Julio NÂ° 150','A','13-03-1999','140406',20),
 ('Yuliana Juana','Saavedra Tacza','y.saavedra@gmail.com','991932289', '70782064','Av. 2 de mayo','A','14-04-2000','140406',21),
-('Jenny','Alfaro Urdaneta','j.alfaro@gmail.com','991932290', '70782065','Av. ramos larrea N° 101','A','13-04-1999','140406',22),
-('Josimar','Bernal Casas','j.bernal@gmail.com','991932291', '70782066','Av. sucre N° 90','A','13-03-1999','140406',23),
-('Rocio','Osorio Malpartida','r.osorio@gmail.com','991932292', '70782067','Av. ramos N° 800','A','13-03-1999','140406',24),
-('Marta','Ramos Ramos','m.ramos@gmail.com','991932293', '70782068','Av. Ayacucho N° 190','A','13-03-1999','140410',25),
-('Raul Alexis','Sanchez Flores','r.sanchez@gmail.com','991932294', '70782069','Av. la mar N° 200','A','13-03-1999','140410',26),
-('Raul Ronny','Sanchez Flores ','r.sanchez@gmail.com','991932295', '70782070','Av. Canada N° 100','A','13-03-1999','140409',27),
-('Karina Lisset','Sanchez Ciprian','k.sanchez@gmail.com','991932296', '70782071','Av. ayacucho N° 140','A','13-03-1999','140409',28),
-('Ernesto Ignacio','Pomalaya Sulca','e.pomalaya@gmail.com','991932297', '70782072','Av. principal N° 401','A','13-03-1999','140409',29);
+('Jenny','Alfaro Urdaneta','j.alfaro@gmail.com','991932290', '70782065','Av. ramos larrea NÂ° 101','A','13-04-1999','140406',22),
+('Josimar','Bernal Casas','j.bernal@gmail.com','991932291', '70782066','Av. sucre NÂ° 90','A','13-03-1999','140406',23),
+('Rocio','Osorio Malpartida','r.osorio@gmail.com','991932292', '70782067','Av. ramos NÂ° 800','A','13-03-1999','140406',24),
+('Marta','Ramos Ramos','m.ramos@gmail.com','991932293', '70782068','Av. Ayacucho NÂ° 190','A','13-03-1999','140410',25),
+('Raul Alexis','Sanchez Flores','r.sanchez@gmail.com','991932294', '70782069','Av. la mar NÂ° 200','A','13-03-1999','140410',26),
+('Raul Ronny','Sanchez Flores ','r.sanchez@gmail.com','991932295', '70782070','Av. Canada NÂ° 100','A','13-03-1999','140409',27),
+('Karina Lisset','Sanchez Ciprian','k.sanchez@gmail.com','991932296', '70782071','Av. ayacucho NÂ° 140','A','13-03-1999','140409',28),
+('Ernesto Ignacio','Pomalaya Sulca','e.pomalaya@gmail.com','991932297', '70782072','Av. principal NÂ° 401','A','13-03-1999','140409',29);
 Go
 
 -- Table: VENTA
@@ -298,8 +299,8 @@ select p.NOMPRO as Pizzas , s.NOMSU as Sucursal from PRODUCTO p
 inner join VENTA_DETALLE vd on vd.CODPRO = p.CODPRO
 inner join venta v on v.IDVEN = vd.IDVEN
 inner join SUCURSAL s on s.IDSU = v.IDSU;
-
 Go
+
 Create view vDetalle as
 select 
 v.IDVEN,NOMSU as SUCURSAL, concat(c.NOMCLI,' , ',c.APECLI) as Cliente,
@@ -311,7 +312,29 @@ inner join venta v on v.IDVEN = vd.IDVEN
 inner join SUCURSAL s on s.IDSU = v.IDSU
 inner join UBIGEO u on u.CODUBI = s.CODUBI
 inner join Cliente c on c.IDCLI = v.IDCLI
-inner join VENDEDOR vend on vend.IDVEND = v.IDVEND
+inner join VENDEDOR vend on vend.IDVEND = v.IDVEND;
+Go
+
+create view boleta as 
+select v.IDVEN,
+concat(c.NOMCLI , ' , ', c.APECLI) as Cliente,
+concat(ve.NOMVEND , ' , ' , ve.APEVEND) as Vendedor ,
+p.NOMPRO,  p.PREPRO, 
+vd.CANTPRO,
+SUBTOTAL = (p.PREPRO * vd.CANTPRO)
+from VENTA_DETALLE vd 
+inner join venta v on v.IDVEN = vd.IDVEN
+inner join PRODUCTO p on p.CODPRO = vd.CODPRO
+inner join cliente c on c.IDCLI = v.IDCLI
+inner join VENDEDOR ve on ve.IDVEND = v.IDVEND ;
+Go
+
+Create view pActivo as
+select * from producto where ESTPRO = 'A';
+Go
+
+Create view pInactivo as
+select * from producto where ESTPRO = 'I';
 Go
 
 -- PROCEDURE ACTUALIZAR PRODUCTO
